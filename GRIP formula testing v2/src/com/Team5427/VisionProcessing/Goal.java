aa54b9e28c37bdc18c61901029cc3e41111c5994
Andrew Kennedy <andrewkennedyftw@gmail.com>
2016-02-05 14:16:37 -0600

    made some more misc. changes to the goal class
    test
    
    Revert "made some more misc. changes to the goal class"
    
    This reverts commit e63c320d089b6ba38439a0941dd62e37718a0337.
    
    Revert "Revert "made some more misc. changes to the goal class""
    
    This reverts commit 59e26a6fdda58ff3e3916085842e36a293dee733.
    
    Revert "test"
    
    This reverts commit 8bbf7b9f63573d894dc5b6840bcd77f1164ac11b.

--- GRIP formula testing v2/src/com/Team5427/VisionProcessing/Goal.java
+++ GRIP formula testing v2/src/com/Team5427/VisionProcessing/Goal.java
@@ -1,7 +1,5 @@
 package com.Team5427.VisionProcessing;
 
-import java.util.ArrayList;
-
 public class Goal {
 
 	public static final double MIN_VERTICAL_SLOPE = -5;
@@ -25,61 +23,15 @@ public class Goal {
 
 	private Line leftLine, centerLine, rightLine;
 
-	private Line temp1, temp2;
-
-	private Line[] lines;
-
 	private double distanceToGoal = -1;
 	private double distanceToTower = -1;
 	private double angleOfElevation = -1;
 	private double area = -1;
 
 	private boolean goalCompleted = false;
-	private boolean isValid = false;
-
-	/**
-	 * Used when only two of the three lines that will comprise the Goal are
-	 * known.
-	 * 
-	 * @param temp1
-	 *            One of the two lines, with no designation as to whether it is
-	 *            the left, right, or bottom line in the U shaped Goal.
-	 * @param temp2
-	 *            One of the two lines, with no designation as to whether it is
-	 *            the left, right, or bottom line in the U shaped Goal.
-	 */
-	public Goal(Line temp1, Line temp2) {
-		this.temp1 = temp1;
-		this.temp2 = temp2;
-		System.out.println("made a Goal!!"); // TODO delete when done
-	}
-
-	/**
-	 * Used when all three of the lines that will comprise a Goal are known.
-	 * 
-	 * @param leftLine
-	 * @param centerLine
-	 * @param rightLine
-	 */
-	/*
-	 * public Goal(Line leftLine, Line centerLine, Line rightLine) {
-	 * this.leftLine = leftLine; this.centerLine = centerLine; this.rightLine =
-	 * rightLine;
-	 * 
-	 * Line[] newLines = new Line[3];
-	 * 
-	 * newLines[0] = leftLine; newLines[1] = rightLine; newLines[2] =
-	 * centerLine;
-	 * 
-	 * this.lines = newLines;
-	 * 
-	 * goalCompleted = true; }
-	 */
 
 	public Goal(Line[] lines) {
-		// System.out.println("making a Goal"); // TODO remove once the program
-		// is
-		// working
+
 		Line[] vertLines = new Line[2];
 
 		int index = 0;
@@ -89,104 +41,46 @@ public class Goal {
 
 		for (int i = 0; i < lines.length; i++) {
 			if (lines[i].isHorizontal()) {
+
 				centerLine = lines[i];
 				setCenter = true;
+
 			} else
 				vertLines[index++] = lines[i];
 		}
 
 		if (vertLines[0].getSmallestX() < vertLines[1].getLargestX()) {
+
 			leftLine = vertLines[0];
 			rightLine = vertLines[1];
 
-//			System.out.println(vertLines[0].toString() + "    \n    " + vertLines[1].toString() + "\n\n");
-
-			// System.out.println("the left line is: "+
-			// vertLines[0].getSmallestX()+", and the right line is: "+
-			// vertLines[1].getLargestX());
 		} else {
+
 			leftLine = vertLines[1];
 			rightLine = vertLines[0];
-			
+
 		}
 
 		if (setCenter = false)
 			goalCompleted = false;
 		else
 			goalCompleted = true;
-		
-		area = leftLine.getLength()*centerLine.getLength();
 
-		/*
-		 * filterVerticalLines(lines); filterHorizontalLines(lines);
-		 * 
-		 * Line[] newLines = new Line[3];
-		 * 
-		 * newLines[0] = leftLine; newLines[1] = rightLine; newLines[2] =
-		 * centerLine;
-		 * 
-		 * this.lines = newLines;
-		 * 
-		 * goalCompleted = true;
-		 */
+		area = leftLine.getLength() * centerLine.getLength();
 	}
 
-	public double getArea() {
-		return area;
-	}
-
-	public Line[] getLines() {
-		return lines;
-	}
-
-	public Line getLeftLine() {
-		return leftLine;
-	}
-
-	public void setLeftLine(Line leftLine) {
-		this.leftLine = leftLine;
-	}
-
-	public Line getCenterLine() {
-		return centerLine;
-	}
-
-	public void setCenterLine(Line centerLine) {
-		this.centerLine = centerLine;
-	}
-
-	public Line getRightLine() {
-		return rightLine;
-	}
-
-	public void setRightLine(Line rightLine) {
-		this.rightLine = rightLine;
-	}
-
-	public Line getTemp1() {
-		return temp1;
-	}
-
-	public void setTemp1(Line temp1) {
-		this.temp1 = temp1;
-	}
-
-	public Line getTemp2() {
-		return temp2;
-	}
-
-	public void setTemp2(Line temp2) {
-		this.temp2 = temp2;
-	}
-
-	public boolean isGoalCompleted() {
-		return goalCompleted;
-	}
-
-	public void setGoalCompleted(boolean goalCompleted) {
-		this.goalCompleted = goalCompleted;
-	}
-
+	/**
+	 * 
+	 * Determines whether or not the current goal is inside of another goal
+	 * based on whether the left and right line of the current goal are within
+	 * the bounds of the left and right lines of the goal that was given to it
+	 * when called.
+	 * 
+	 * @param g
+	 *            The goal which is potentially outside of the current goal.
+	 * @return Whether or not the current goal is inside of the goal passed
+	 *         through the parameters.
+	 */
 	public boolean isInsideGoal(Goal g) {
 		if (g.leftLine.getSmallestX() > leftLine.getSmallestX()
 				&& g.rightLine.getLargestY() < rightLine.getLargestX()) {
@@ -198,100 +92,46 @@ public class Goal {
 	}
 
 	/**
-	 * Filters the vertical lines to appropriate left and right line for the
-	 * goal
-	 *
-	 * @param lineList
-	 *            Lines within the goal
+	 * Method to access the horizontal line in the goal that
+	 * 
+	 * @return The center line of the goal.
 	 */
-	public void filterVerticalLines(Line[] lineList) {
- 
-		ArrayList<Line> verticalLines = new ArrayList<>();
-
-		/* Filters all vertical lines that meets the criteria */
-		for (Line l : lineList) {
-			double slope = l.getSlope();
-			// System.out.println("Slope of line: " + slope + " - is horizontal:
-			// " + (MIN_HORIZONTAL_SLOPE < slope && MAX_HORIZONTAL_SLOPE >
-			// slope));
-
-			if (MIN_VERTICAL_SLOPE > slope || MAX_VERTICAL_SLOPE < slope)
-				verticalLines.add(l);
-		}
-
-		/* Finds the left line */
-		leftLine = verticalLines.get(0);
-		for (int i = 1; i < verticalLines.size(); i++) {
-			if (verticalLines.get(i).getSmallestX() < leftLine.getSmallestX())
-				leftLine = verticalLines.get(i);
-		}
-
-		/* Finds the right line */
-		rightLine = verticalLines.get(0);
-		for (int i = 1; i < verticalLines.size(); i++) {
-			if (verticalLines.get(i).getLargestX() > rightLine.getLargestX())
-				rightLine = verticalLines.get(i);
-		}
-
+	public Line getCenterLine() {
+		return centerLine;
 	}
 
-	/**
-	 * Finds the bottom line of the target and sets it as the goal's bottom line
-	 *
-	 * @param lineList
-	 *            lines within the goal
-	 */
-	public void filterHorizontalLines(Line[] lineList) {
-
-		// System.out.println(lineList.length);
-
-		try {
-
-			ArrayList<Line> horizontalLines = new ArrayList<>();
-
-			/* Filters all vertical lines that meets the criteria */
-			for (Line l : lineList) {
-				// System.out.println(l);
-				double slope = l.getSlope();
-
-				// System.out.println("Slope of line: " + slope + " - is
-				// horizontal: " + (MIN_HORIZONTAL_SLOPE < slope &&
-				// MAX_HORIZONTAL_SLOPE > slope));
-
-				if (MIN_HORIZONTAL_SLOPE < slope || MAX_HORIZONTAL_SLOPE > slope)
-					horizontalLines.add(l);
-			}
-
-			/* Finds the bottom line */
-			centerLine = horizontalLines.get(0);
-			for (int i = 1; i < horizontalLines.size(); i++) {
-				if (horizontalLines.get(i).getLargestY() > centerLine.getLargestY())
-					centerLine = horizontalLines.get(i);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		if (centerLine == null)
-			System.err.println("Whoops! centerline is null");
+	public void setCenterLine(Line centerLine) {
+		this.centerLine = centerLine;
 	}
 
-	/**
-	 * TODO: Needs testing, not sure if this works yet Determines if current
-	 * goal is a valid for shooting
-	 */
-	public void determineValid() {
-		if (leftLine.getMidpointY() > centerLine.getMidpointY() || rightLine.getMidpointY() < centerLine.getMidpointY())
-			return;
-
-		isValid = true;
+	public Line getLeftLine() {
+		return leftLine;
 	}
 
-	/**
-	 * Returns the distance from the goal to the robot
-	 *
-	 * @return the distance from goal to the robot
-	 */
+	public void setLeftLine(Line leftLine) {
+		this.leftLine = leftLine;
+	}
+
+	public Line getRightLine() {
+		return rightLine;
+	}
+
+	public void setRightLine(Line rightLine) {
+		this.rightLine = rightLine;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public boolean isGoalCompleted() {
+		return goalCompleted;
+	}
+
+	public void setGoalCompleted(boolean goalCompleted) {
+		this.goalCompleted = goalCompleted;
+	}
+
 	public double getDistanceToRobot() {
 
 		if (distanceToGoal > 0)
@@ -313,24 +153,18 @@ public class Goal {
 		if (distanceToTower > 0)
 			return distanceToTower;
 
-		distanceToTower = (TOWER_HEIGHT - ROBOT_HEIGHT) / Math.tan(getAngleElevationRadians());
+		distanceToTower = (TOWER_HEIGHT - ROBOT_HEIGHT) / Math.tan(getAngleOfELevationInRadians());
 
 		return distanceToTower;
 	}
 
 	/**
-	 * Returns the angle of elevation from the target to the robot in radians
-	 * 
-	 * @return the angle of the robot to the goal in radians
+	 * @return The angle of the robot to the goal in radians
 	 */
-	public double getAngleElevationRadians() {
+	public double getAngleOfELevationInRadians() {
 
 		if (angleOfElevation > 0)
 			return angleOfElevation;
-
-		// System.err.println(TOWER_HEIGHT);
-		// System.err.println(ROBOT_HEIGHT);
-		// System.err.println(getDistanceToRobot());
 
 		angleOfElevation = Math.asin((TOWER_HEIGHT - ROBOT_HEIGHT) / getDistanceToRobot());
 
@@ -338,11 +172,9 @@ public class Goal {
 	}
 
 	/**
-	 * Returns the angle of elevation from the target to the robot in degrees
-	 * 
 	 * @return the angle of the robot to the goal in degrees
 	 */
-	public double getAngleElevationDegrees() {
-		return Math.toDegrees(getAngleElevationRadians());
+	public double getAngleOfElevationInDegrees() {
+		return Math.toDegrees(getAngleOfELevationInRadians());
 	}
 }
