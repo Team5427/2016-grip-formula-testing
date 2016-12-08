b6b08e2f720481c650538ff0ec4695f4aff3ae7a
Charlemagne Wong <frian13@hotmail.com>
2016-12-07 19:20:11 -0600

    misc changes

--- GRIP formula testing v2/src/com/Team5427/VisionProcessing/GraphicsPanel.java
+++ GRIP formula testing v2/src/com/Team5427/VisionProcessing/GraphicsPanel.java
@@ -355,7 +355,7 @@ public class GraphicsPanel extends JPanel implements KeyListener {
 			bg.drawString("Horizontal Angle: " + horizontalAngle + "°", x, y += interval);
 			bg.drawString("Shooting Range: ", x, y);
 			bg.drawString("                " + Main.goals.get(i).getAngleStatus(), x, y += interval);
-			
+
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
